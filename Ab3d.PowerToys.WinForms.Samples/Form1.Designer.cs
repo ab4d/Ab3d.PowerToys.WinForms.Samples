00d8ecd0c601f1eca5c209e301c36dc12781869d
Andrej Benedik <abenedik@ab4d.com>
2020-12-21 12:01:28 +0100

    Initial source code commit

---
+++ Ab3d.PowerToys.WinForms.Samples/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Ab3d.PowerToys.WinForms.Samples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.animateButton = new System.Windows.Forms.Button();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 643);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1315, 60);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.clearButton);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.animateButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(288, 643);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show Form2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 612);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Move camera with CTRL + right button";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 590);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rotate camera with right mouse button";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 568);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click to select boxes";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(17, 103);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(251, 28);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "CLEAR";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(17, 139);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(249, 424);
+            this.textBox1.TabIndex = 1;
+            // 
+            // animateButton
+            // 
+            this.animateButton.Location = new System.Drawing.Point(16, 68);
+            this.animateButton.Margin = new System.Windows.Forms.Padding(4);
+            this.animateButton.Name = "animateButton";
+            this.animateButton.Size = new System.Drawing.Size(251, 28);
+            this.animateButton.TabIndex = 0;
+            this.animateButton.Text = "Stop animation";
+            this.animateButton.UseVisualStyleBackColor = true;
+            this.animateButton.Click += new System.EventHandler(this.animateButton_Click);
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.elementHost1.Location = new System.Drawing.Point(288, 0);
+            this.elementHost1.Margin = new System.Windows.Forms.Padding(4);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(1027, 643);
+            this.elementHost1.TabIndex = 1;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = null;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(13, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(737, 40);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "This sample is demonstrating how to show WPF with Ab3d.PowerToys in a WinForms ap" +
+    "plication.\r\nTo see a demonstration of Ab3d.PowerToys features please open the WP" +
+    "F samples project.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 703);
+            this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Ab3d.PowerToys in WinForms (defined in code)";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button animateButton;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ab3d.PowerToys.WinForms.Samples/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+using Ab3d.Cameras;
+using Ab3d.Common.Cameras;
+using Ab3d.Common.EventManager3D;
+using Ab3d.Controls;
+using Ab3d.Utilities;
+using HorizontalAlignment = System.Windows.HorizontalAlignment;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+using Point = System.Drawing.Point;
+
+namespace Ab3d.PowerToys.WinForms.Samples
+{
+    public partial class Form1 : Form
+    {
+        // This sample shows how easy is to use WPF inside WinForms application.
+        // It also shows how easy is to create interesting 3D scene with Ab3d.PowerToys library.
+
+        private Viewport3D _viewport3D;
+        private TargetPositionCamera _targetPositionCamera;
+        private MouseCameraController _mouseCameraController;
+        private Grid _rootGrid;
+        private EventManager3D _eventManager3D;
+
+        private bool _isSelectedBoxClicked;
+
+        private double _totalClickedHeight;
+
+        private DiffuseMaterial _normalMaterial = new DiffuseMaterial(System.Windows.Media.Brushes.Silver);
+        private DiffuseMaterial _selectedMaterial = new DiffuseMaterial(System.Windows.Media.Brushes.Orange);
+        private DiffuseMaterial _clickedMaterial = new DiffuseMaterial(System.Windows.Media.Brushes.Red);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetUpWpf3D();
+            Setup3DObjects();
+
+            // HACK HACK HACK:
+            // To pass mouse wheel events to WPF controls, we need to manually pass the mouse wheel event to WPF
+            // The following class does that for us:
+            MouseWheelMessageFilter.RegisterMouseWheelHandling(_rootGrid);
+        }
+
+
+        private void Setup3DObjects()
+        {
+            // The event manager will be used to manage the mouse events on our boxes
+            _eventManager3D = new Ab3d.Utilities.EventManager3D(_viewport3D);
+
+
+            // Add a wire grid
+            var wireGridVisual3D = new Ab3d.Visuals.WireGridVisual3D()
+            {
+                Size = new System.Windows.Size(1000, 1000),
+                HeightCellsCount = 10,
+                WidthCellsCount = 10,
+                LineThickness = 3
+            };
+
+            _viewport3D.Children.Add(wireGridVisual3D);
+
+
+            // Create 7 x 7 boxes with different height
+            for (int y = -3; y <= 3; y++)
+            {
+                for (int x = -3; x <= 3; x ++)
+                {
+                    // Height is based on the distance from the center
+                    double height = (5 - Math.Sqrt(x * x + y * y)) * 60;
+
+                    // Create the 3D Box visual element
+                    var boxVisual3D = new Ab3d.Visuals.BoxVisual3D()
+                    {
+                        CenterPosition = new Point3D(x * 100, height / 2, y * 100),
+                        Size = new Size3D(80, height, 80),
+                        Material = _normalMaterial
+                    };
+
+                    _viewport3D.Children.Add(boxVisual3D);
+
+
+                    // With EventManager we can subscribe to mouse events as we would have standard 2D controls:
+                    var visualEventSource3D = new VisualEventSource3D(boxVisual3D);
+                    visualEventSource3D.MouseEnter += BoxOnMouseEnter;
+                    visualEventSource3D.MouseLeave += BoxOnMouseLeave;
+                    visualEventSource3D.MouseClick += BoxOnMouseClick;
+
+                    _eventManager3D.RegisterEventSource3D(visualEventSource3D);
+                }
+            }
+
+            ToggleCameraAnimation(); // Start camer animation
+        }
+
+        private void BoxOnMouseClick(object sender, MouseButton3DEventArgs mouseButton3DEventArgs)
+        {
+            // HitObject is our BoxVisual3D
+            var boxVisual3D = mouseButton3DEventArgs.HitObject as Ab3d.Visuals.BoxVisual3D;
+            if (boxVisual3D == null)
+                return; // This should not happen
+
+            // Toggle clicked and normal material
+            if (!_isSelectedBoxClicked)
+            {
+                boxVisual3D.Material = _clickedMaterial;
+                _isSelectedBoxClicked = true;
+
+                _totalClickedHeight += boxVisual3D.Size.Y;
+            }
+            else
+            {
+                boxVisual3D.Material = _normalMaterial;
+                _isSelectedBoxClicked = false;
+
+                _totalClickedHeight -= boxVisual3D.Size.Y;
+            }
+
+            UpdateTotalClickedHeightText();
+        }
+
+        private void BoxOnMouseEnter(object sender, Mouse3DEventArgs mouse3DEventArgs)
+        {
+            var boxVisual3D = mouse3DEventArgs.HitObject as Ab3d.Visuals.BoxVisual3D;
+            if (boxVisual3D == null)
+                return; // This should not happen
+
+            // Set _isSelectedBoxClicked to true if the selected box is clicked (red) - this will be used on MouseLeave
+            _isSelectedBoxClicked = ReferenceEquals(boxVisual3D.Material, _clickedMaterial);
+
+            boxVisual3D.Material = _selectedMaterial;
+        }
+
+        private void BoxOnMouseLeave(object sender, Mouse3DEventArgs mouse3DEventArgs)
+        {
+            var boxVisual3D = mouse3DEventArgs.HitObject as Ab3d.Visuals.BoxVisual3D;
+            if (boxVisual3D == null)
+                return; // This should not happen
+
+            if (_isSelectedBoxClicked)
+                boxVisual3D.Material = _clickedMaterial;
+            else
+                boxVisual3D.Material = _normalMaterial;
+        }
+
+        private void UpdateTotalClickedHeightText()
+        {
+            textBox1.Text = string.Format("Total clicked height: {0:0}\r\n{1}", _totalClickedHeight, textBox1.Text);
+        }
+
+
+        private void SetUpWpf3D()
+        {
+            // The following controls are usually defined in XAML in Wpf project
+            // But here we can also define them in code.
+
+            // We need a root grid because we will host more than one control
+            _rootGrid = new Grid();
+            _rootGrid.Background = System.Windows.Media.Brushes.White;
+
+
+            // Viewport3D is a WPF control that can show 3D graphics
+            _viewport3D = new Viewport3D();
+            _rootGrid.Children.Add(_viewport3D);
+
+
+            // Specify TargetPositionCamera that will show our 3D scene
+
+            //<cameras:TargetPositionCamera Name="Camera1"
+            //                    Heading="30" Attitude="-20" Bank="0" 
+            //                    Distance="1300" TargetPosition="0 0 0" 
+            //                    ShowCameraLight="Always"
+            //                    TargetViewport3D="{Binding ElementName=MainViewport}"/>
+
+            _targetPositionCamera = new Ab3d.Cameras.TargetPositionCamera()
+            {
+                TargetPosition = new Point3D(0, 0, 0),
+                Distance = 1300,
+                Heading = 30,
+                Attitude = -20,
+                ShowCameraLight = ShowCameraLightType.Always,
+                TargetViewport3D = _viewport3D
+            };
+
+            _rootGrid.Children.Add(_targetPositionCamera);
+
+
+            // Set rotate to right mouse button
+            // and move to CRTL + right mouse button
+            // Left mouse button is left for clicking on the 3D objects
+
+            //<controls:MouseCameraController Name="MouseCameraController1"
+            //                                RotateCameraConditions="RightMouseButtonPressed"
+            //                                MoveCameraConditions="ControlKey, RightMouseButtonPressed" 
+            //                                EventsSourceElement="{Binding ElementName=RootViewportBorder}"
+            //                                TargetCamera="{Binding ElementName=Camera1}"/>
+
+            _mouseCameraController = new Ab3d.Controls.MouseCameraController()
+            {
+                RotateCameraConditions = MouseCameraController.MouseAndKeyboardConditions.RightMouseButtonPressed,
+                MoveCameraConditions = MouseCameraController.MouseAndKeyboardConditions.RightMouseButtonPressed | MouseCameraController.MouseAndKeyboardConditions.ControlKey,
+                EventsSourceElement = _rootGrid,
+                TargetCamera = _targetPositionCamera
+            };
+
+            _rootGrid.Children.Add(_mouseCameraController);
+
+
+            // Show buttons that can be used to rotate and move the camera
+
+            //<controls:CameraControlPanel VerticalAlignment="Bottom" HorizontalAlignment="Right" Margin="5" Width="225" Height="75" ShowMoveButtons="True"
+            //                                TargetCamera="{Binding ElementName=Camera1}"/>
+
+            var cameraControlPanel = new Ab3d.Controls.CameraControlPanel()
+            {
+                VerticalAlignment = VerticalAlignment.Bottom,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Margin = new Thickness(5, 5, 5, 5),
+                Width = 225,
+                Height = 75,
+                ShowMoveButtons = true,
+                TargetCamera = _targetPositionCamera
+            };
+
+            _rootGrid.Children.Add(cameraControlPanel);
+
+
+            // Finally add the root WPF Grid to elementHost1
+            elementHost1.Child = _rootGrid;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            form2.Closed += (o, args) => this.Show();
+
+            this.Hide();
+            form2.Show();
+        }
+
+        private void animateButton_Click(object sender, EventArgs e)
+        {
+            ToggleCameraAnimation();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            foreach (var boxVisual3D in _viewport3D.Children.OfType<Ab3d.Visuals.BoxVisual3D>())
+                boxVisual3D.Material = _normalMaterial;
+
+            _totalClickedHeight = 0;
+            UpdateTotalClickedHeightText();
+        }
+
+        private void ToggleCameraAnimation()
+        {
+            if (_targetPositionCamera.IsRotating)
+            {
+                _targetPositionCamera.StopRotation();
+                animateButton.Text = "Start animation";
+            }
+            else
+            {
+                _targetPositionCamera.StartRotation(10, 0); // animate the camera with changing heading for 10 degrees in one second
+                animateButton.Text = "Stop animation";
+            }
+        }
+    }
+}
